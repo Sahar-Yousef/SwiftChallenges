326c29b2e1a77552ce7d3c3fa116c7774034bdd1
Sahar-Yousef <Sell.lla.78@gmail.com>
2023-10-05 20:39:25 +0300

    Initial Commit

---
+++ WaterChallenge/WaterChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterChallengeApp.swift
+//  WaterChallenge
+//
+//  Created by Sahora on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct WaterChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
